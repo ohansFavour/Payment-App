a5b6b64eb2d2eb25a375c424d8f0c3d945fd5fbc
Favour Ohans <fohanekwu@gmail.com>
2020-07-19 11:49:09 +0100

    fixed payment complete bug

--- src/pages/completed/completed.jsx
+++ src/pages/completed/completed.jsx
@@ -7,27 +7,21 @@ const Completed = () => {
   return (
     <div className="completed__container">
       <div className="completed__content">
-        <div className="completed__mark">
-          <span
-            className="iconify"
-            data-icon="flat-color-icons:checkmark"
-            data-inline="false"
-          ></span>
-        </div>
-        <div className="completed__main">
-          <div>
-            <p className="purchase">Purchase Completed</p>
-            <p className="purchase__details">
-              Please check your email for details concerning this transaction
-            </p>
-            <div className="link__container">
-              <Link to="/">Return Home</Link>
-            </div>
-          </div>
+      <h1><span
+          className="iconify"
+          data-icon="flat-color-icons:checkmark"
+          data-inline="false"
+          align="center"
+        ></span></h1>
+        <span className="purchase">Purchase Completed</span>
+        <span className="purchase__details">
+          Please check your email for details concerning this transaction
+        </span>
+        <div className="link__container">
+          <Link to="/">Return Home</Link>
         </div>
       </div>
-    </div>
-  );
+    </div>)
 };
 
 export default Completed;
